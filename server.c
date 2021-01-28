c1317d7ea607cbaa3848f425e4b2bb05569aaf53
Klemens Träger <72913784+klemenstraeger@users.noreply.github.com>
2021-01-28 15:18:43 +0100

    Update server.c

--- server.c
+++ server.c
@@ -2,8 +2,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include "Struct.h"
-
-//Wenn Programma auf Linux
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -12,14 +10,14 @@
 
 int main(int argc, char *argv[])
 {
-	printf("\n\nInitlizing Server...\n\n");
 	int port = atoi(argv[1]);
 	char *sNummer = argv[2];
+	printf("\n\nInitlizing Server...\n\n");
 	printf("Name: %s\nPort: %d\n", sNummer, port);
-	int clielen, recvlen;
-	
-	struct sockaddr_in6 serveraddr, clientaddr;
 
+	int clielen, recvlen;
+
+	struct sockaddr_in6 serveraddr, clientaddr;
 	int listeningsocket = socket(AF_INET6, SOCK_STREAM, 0);
 
 	if (listeningsocket < 0)
@@ -29,7 +27,6 @@ int main(int argc, char *argv[])
 	}
 
 	memset(&serveraddr, 0, sizeof(serveraddr));
-
 	serveraddr.sin6_family = AF_INET6;
 	serveraddr.sin6_addr = in6addr_any;
 	serveraddr.sin6_port = htons(7000);
@@ -41,12 +38,12 @@ int main(int argc, char *argv[])
 	}
 
 	listen(listeningsocket, 30);
-
 	clielen = sizeof(clientaddr);
 
-	int newsock = accept4(listeningsocket, (struct sockaddr *) &clientaddr, &clielen, SOCK_NONBLOCK);
+	//Nonblocking socket
+	int incsock = accept4(listeningsocket, (struct sockaddr *) &clientaddr, &clielen, SOCK_NONBLOCK);
 
-	if (newsock < 0)
+	if (incsock < 0)
 	{
 		perror("Accept error");
 		return -1;
@@ -54,96 +51,87 @@ int main(int argc, char *argv[])
 
 	struct packet incommingmsg;
 	char recvbuf[sizeof(struct packet)];
-	char sendbuf[BUFFERSIZE];
-	int txtlen;
 
-	recvlen = recv(newsock, recvbuf, sizeof(struct packet), 0);
+	recvlen = recv(incsock, recvbuf, sizeof(struct packet), 0);
 	if (recvlen < 0)
 	{
-		perror("REcv Error error");
+		perror("Recv Error error");
 		return -1;
 	}
 
 	memcpy(&incommingmsg, recvbuf, sizeof(recvbuf));
-	
 	printf("Verbunden mit: %s\n", incommingmsg.snummer);
 	fflush(stdout);
-	
+
 	printf("Beginne einen Chat...\n:");
 	fflush(stdout);
-	
-	int first =1;
 
 	fd_set readfds;
 
 	for (;;)
 	{
 		FD_ZERO(&readfds);
-		FD_SET(newsock, &readfds);
+		FD_SET(incsock, &readfds);
 		FD_SET(0, &readfds);
-		
+
 		fflush(stdout);
 
-		if (select(newsock + 1, &readfds, NULL, NULL, NULL) < 0)
+		if (select(incsock + 1, &readfds, NULL, NULL, NULL) < 0)
 		{
 			perror("select error");
 		}
 
-		if (FD_ISSET(newsock, &readfds))
+		//Eigehende Nachicht von Client
+		if (FD_ISSET(incsock, &readfds))
 		{
 			struct packet incommingmsg;
 			char recvbuf[sizeof(struct packet)];
-			char sendbuf[BUFFERSIZE];
-			int txtlen;
 
-			recvlen = recv(newsock, recvbuf, sizeof(struct packet), 0);
+			recvlen = recv(incsock, recvbuf, sizeof(struct packet), 0);
 			if (recvlen < 0)
 			{
 				printf("RECV ERROR IPV6:");
 				return -1;
 			}
-         if(recvlen  == 0){
-            printf("Client disconnected!");
-            break;
-         }
+
+			if (recvlen == 0)
+			{
+				printf("Client disconnected!");
+				break;
+			}
 
 			memcpy(&incommingmsg, recvbuf, sizeof(recvbuf));
-			
-			printf("%s> %s", incommingmsg.snummer, incommingmsg.text);
+			printf("%s > %s", incommingmsg.snummer, incommingmsg.text);
 			fflush(stdout);
 
 		}
 
 		if (FD_ISSET(0, &readfds))
 		{
-
-   
-
 			char buf[BUFFERSIZE];
-
 			fgets(buf, BUFFERSIZE, stdin);
+
 			struct packet message;
 			strcpy(message.snummer, sNummer);
 			strcpy(message.text, buf);
-			int sendret = send(newsock, &message, sizeof(struct packet), 0);
+
+			int sendret = send(incsock, &message, sizeof(struct packet), 0);
 
 			if (sendret != sizeof(struct packet))
 			{
-				printf("SEND ERROR IPV6:");
+				printf("SEND ERROR:");
 				return -1;
 			}
 
 			if (sendret == -1)
 			{
+				printf("Client disconected!")
 				break;
 			}
 		}
-
-		//RECEIVE MESSAGE
-
 	}
 
-	close(newsock);
+	close(incsock);
 	close(listeningsocket);
-	return 0;
+	return 1;
 }
